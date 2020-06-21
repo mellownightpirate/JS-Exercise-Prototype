7b83b540da573a2b8f5f7aed6ce6793b7fbc795d
mellownightpirate <a@hasan.co>
2020-06-21 22:41:35 +0100

    Task 2 car instances and stretch

--- index.js
+++ index.js
@@ -70,8 +70,25 @@ Person.prototype.toString = function() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, mpg) {
+  this.model = model;
+  this.milesPerGallon = mpg;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function (gallons) {
+  this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function (distance) {
+  const drivableMiles = this.tank * this.milesPerGallon;
+  if (distance <= drivableMiles) {
+  this.odometer = this.odometer + distance;
+  this.tank = this.tank - (distance / this.milesPerGallon)
+} else {
+  this.odometer = this.odometer + drivableMiles;
+  this.tank = 0;
+  return `I ran out of fuel at ${this.odometer} miles!`
+  }
 }
 
 /*
